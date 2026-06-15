d640c03dbf7baea1f88ecc7bc9dd6d769a97b3d8
sukacode <firmansahalhadi@gmail.com>
2026-06-15 15:01:56 +0700

    feat: update configuration

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,13 +3,17 @@ package config
 import "os"
 
 type Config struct {
-	BaseURL string
-	APIKey  string
+	BaseURL     string
+	CorporateID string
+	ClientID    string
+	ClientSecret string
 }
 
 func Load() *Config {
 	return &Config{
-		BaseURL: os.Getenv("TIKET_BASE_URL"),
-		APIKey:  os.Getenv("TIKET_API_KEY"),
+		BaseURL:      os.Getenv("TIKET_BASE_URL"),
+		CorporateID:  os.Getenv("TIKET_CORPORATE_ID"),
+		ClientID:     os.Getenv("TIKET_CLIENT_ID"),
+		ClientSecret: os.Getenv("TIKET_CLIENT_SECRET"),
 	}
 }
